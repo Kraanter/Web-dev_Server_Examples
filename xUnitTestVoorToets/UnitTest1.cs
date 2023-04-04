d235e41af99514d3e0279119c592379e89905458
krijn <grimmekrijn@gmail.com>
2023-04-04 21:00:23 +0200

    Initial commit

---
+++ xUnitTestVoorToets/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace xUnitTestVoorToets
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
